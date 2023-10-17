2954bc1dac6f2108d6ff5c72ca0ec998a5abc1cd
Nouman Arshad <noumanarshad15926@gmail.com>
2023-10-17 20:28:24 +0500

    Update AboutSection.jsx

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -53,7 +53,7 @@ const AboutSection = () => {
         <section className='text-white'>
             <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
 
-                <Image src='/about-image.png' width={500} height={500} />
+                <Image src='/about-image.png' alt='about' width={500} height={500} />
                 <div className='md-4 md:mt-0 text-left flex flex-col h-full'>
                     <h1 className='text-4xl font-bold text-white mb-4'>About Me</h1>
                     <p className='text-base lg:text-lg'>
